6cfd99a6f9ff1d47942fb81f7632d503894a9e10
Hilton Perantunes <hperantunes@gmail.com>
2023-12-09 16:00:24 -0500

    Lesson 30

--- scenes/manager/arena_time_manager.gd
+++ scenes/manager/arena_time_manager.gd
@@ -1,12 +1,25 @@
 extends Node
 
+signal arena_difficulty_increased(arena_difficulty: int)
+
+const DIFFICULTY_INTERVAL = 5
+
 @export var end_screen_scene: PackedScene
 
 @onready var timer = $Timer
 
+var arena_diffficulty = 0
+
 
 func _ready():
 	timer.timeout.connect(on_timer_timeout)
+
+
+func _process(_delta):
+	var next_time_target = timer.wait_time - ((arena_diffficulty + 1) * DIFFICULTY_INTERVAL)
+	if timer.time_left <= next_time_target:
+		arena_diffficulty += 1
+		arena_difficulty_increased.emit(arena_diffficulty)
 
 
 func get_time_elapsed():
