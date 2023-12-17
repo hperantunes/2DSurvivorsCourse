594c588d57e623df953a2198ce909d0e773a4f0c
Hilton Perantunes <hperantunes@gmail.com>
2023-12-16 22:06:37 -0500

    Lesson 39

--- scenes/game_object/wizard_enemy/wizard_enemy.gd
+++ scenes/game_object/wizard_enemy/wizard_enemy.gd
@@ -7,8 +7,8 @@ extends CharacterBody2D
 func _process(_delta):
 	velocity_component.accelerate_to_player()
 	velocity_component.move(self)
-	
+
 	var move_sign = sign(velocity.x)
 	if move_sign != 0:
-		visuals.scale = Vector2(-move_sign, 1)
+		visuals.scale = Vector2(move_sign, 1)
 
