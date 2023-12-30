43d64ae8ea4eacbc342d6bfd792966015387123c
Hilton Perantunes <hperantunes@gmail.com>
2023-12-29 19:57:18 -0500

    Lesson 62

---
+++ scenes/ui/main_menu.gd
@@ -0,0 +1,19 @@
+extends CanvasLayer
+
+
+func _ready():
+	$%PlayButton.pressed.connect(on_play_button_pressed)
+	$%OptionsButton.pressed.connect(on_options_button_pressed)
+	$%QuitButton.pressed.connect(on_quit_button_pressed)
+
+
+func on_play_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/main/main.tscn")
+
+
+func on_options_button_pressed():
+	pass
+
+
+func on_quit_button_pressed():
+	get_tree().quit()
