8ce7f883e71131ee03a697e5b904ee87ed0c9738
Hilton Perantunes <hperantunes@gmail.com>
2024-01-01 23:27:10 -0500

    Lesson 76

---
+++ scenes/game_object/bat_enemy/bat_enemy.gd
@@ -0,0 +1,24 @@
+extends CharacterBody2D
+
+@onready var visuals = $Visuals
+@onready var velocity_component = $VelocityComponent
+@onready var hit_random_audio_player_component = $HitRandomAudioPlayerComponent
+
+var is_moving = false
+
+
+func _ready():
+	$HurtboxComponent.hit.connect(on_hit)
+
+
+func _process(_delta):
+	velocity_component.accelerate_to_player()
+	velocity_component.move(self)
+
+	var move_sign = sign(velocity.x)
+	if move_sign != 0:
+		visuals.scale = Vector2(move_sign, 1)
+
+
+func on_hit():
+	hit_random_audio_player_component.play_random()
