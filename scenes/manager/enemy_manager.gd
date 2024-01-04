f56400378429fbdb8b373ac26111050d155ec447
Hilton Perantunes <hperantunes@gmail.com>
2024-01-03 23:47:02 -0500

    Lesson 80

--- scenes/manager/enemy_manager.gd
+++ scenes/manager/enemy_manager.gd
@@ -11,6 +11,7 @@ const SPAWN_RADIUS = 375
 
 var base_spawn_time = 0
 var enemy_table = WeightedTable.new()
+var number_to_spawn = 1
 
 
 func _ready():
@@ -50,12 +51,13 @@ func on_timer_timeout():
 	if player == null:
 		return
 	
-	var enemy_scene = enemy_table.pick_item()
-	var enemy = enemy_scene.instantiate() as Node2D
-	
-	var entities_layer = get_tree().get_first_node_in_group("entities_layer")
-	entities_layer.add_child(enemy)
-	enemy.global_position = get_spawn_position()
+	for i in number_to_spawn:
+		var enemy_scene = enemy_table.pick_item()
+		var enemy = enemy_scene.instantiate() as Node2D
+		
+		var entities_layer = get_tree().get_first_node_in_group("entities_layer")
+		entities_layer.add_child(enemy)
+		enemy.global_position = get_spawn_position()
 
 
 func on_arena_difficulty_increased(arena_difficulty: int):
@@ -68,3 +70,5 @@ func on_arena_difficulty_increased(arena_difficulty: int):
 	elif arena_difficulty == 18:
 		enemy_table.add_item(bat_enemy_scene, 10)
 	
+	if (arena_difficulty % 6) == 0:
+		number_to_spawn += 1
